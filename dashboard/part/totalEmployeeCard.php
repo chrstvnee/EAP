9432fb82613f3e7886650146491f9580ee9c7866
christine <salutillo.tintin@gmail.com>
2024-12-01 21:35:10 +0800

    update dashboard

---
+++ dashboard/part/totalEmployeeCard.php
@@ -0,0 +1,227 @@
+<!--begin::Row-->
+<div class="row g-5 gx-xl-10 mb-5 mb-xl-10">
+    <!--begin::Col-->
+    <div class="col-md-6 col-lg-6 col-xl-6 col-xxl-6 mb-md-10 mb-xl-10">
+        <!--begin::Total Employee-->
+        <div class="card card-flush">
+            <!--begin::Header-->
+            <div class="card-header pt-5">
+                <!--begin::Title-->
+                <div class="card-title d-flex flex-column">
+                    <!--begin::Info-->
+                    <div class="d-flex align-items-center">
+                        <!--begin::Amount-->
+                        <span class="fs-2hx fw-bold text-gray-900 me-2 lh-1 ls-n2">999</span>
+                        <!--end::Amount-->
+                    </div>
+                    <!--end::Info-->
+                    <!--begin::Subtitle-->
+                    <span class="text-gray-500 pt-1 fw-semibold fs-6">Total Employee</span>
+                    <!--end::Subtitle-->
+                </div>
+                <!--end::Title-->
+            </div>
+            <!--end::Header-->
+            <!--begin::Card body-->
+            <div class="card-body pt-2 pb-4 d-flex flex-wrap align-items-center">
+                <!--begin::Chart-->
+                <div class="d-flex flex-center me-5 pt-2">
+                    <div id="kt_card_widget_17_chart" style="min-width: 70px; min-height: 70px" data-kt-size="70"
+                        data-kt-line="11"></div>
+                </div>
+                <!--end::Chart-->
+                <!--begin::Labels-->
+                <div class="d-flex flex-column content-justify-center flex-row-fluid">
+                    <!--begin::Label-->
+                    <div class="d-flex fw-semibold align-items-center">
+                        <!--begin::Bullet-->
+                        <div class="bullet w-8px h-3px rounded-2 bg-success me-3"></div>
+                        <!--end::Bullet-->
+                        <!--begin::Label-->
+                        <div class="text-gray-500 flex-grow-1 me-4">Department A</div>
+                        <!--end::Label-->
+                        <!--begin::Stats-->
+                        <div class="fw-bolder text-gray-700 text-xxl-end">999</div>
+                        <!--end::Stats-->
+                    </div>
+                    <!--end::Label-->
+                    <!--begin::Label-->
+                    <div class="d-flex fw-semibold align-items-center my-3">
+                        <!--begin::Bullet-->
+                        <div class="bullet w-8px h-3px rounded-2 bg-primary me-3"></div>
+                        <!--end::Bullet-->
+                        <!--begin::Label-->
+                        <div class="text-gray-500 flex-grow-1 me-4">Department B</div>
+                        <!--end::Label-->
+                        <!--begin::Stats-->
+                        <div class="fw-bolder text-gray-700 text-xxl-end">999</div>
+                        <!--end::Stats-->
+                    </div>
+                    <!--end::Label-->
+                    <!--begin::Label-->
+                    <div class="d-flex fw-semibold align-items-center">
+                        <!--begin::Bullet-->
+                        <div class="bullet w-8px h-3px rounded-2 me-3" style="background-color: #E4E6EF"></div>
+                        <!--end::Bullet-->
+                        <!--begin::Label-->
+                        <div class="text-gray-500 flex-grow-1 me-4">Others</div>
+                        <!--end::Label-->
+                        <!--begin::Stats-->
+                        <div class="fw-bolder text-gray-700 text-xxl-end">999</div>
+                        <!--end::Stats-->
+                    </div>
+                    <!--end::Label-->
+                </div>
+                <!--end::Labels-->
+            </div>
+            <!--end::Card body-->
+        </div>
+        <!--end::Total Employee-->
+    </div>
+    <!--end::Col-->
+    <!--begin::Col-->
+    <div class="col-md-6 col-lg-6 col-xl-6 col-xxl-6 mb-md-10 mb-xl-10">
+        <!--begin::Employment Status-->
+        <div class="card card-flush">
+            <!--begin::Header-->
+            <div class="card-header pt-5">
+                <!--begin::Title-->
+                <div class="card-title d-flex flex-column">
+                    <!--begin::Info-->
+                    <div class="d-flex align-items-center">
+                        <!--begin::Currency-->
+                        <span class="fs-4 fw-semibold text-gray-500 me-1 align-self-start"></span>
+                        <!--end::Currency-->
+                        <!--begin::Amount-->
+                        <span class="fs-2hx fw-bold text-gray-900 me-2 lh-1 ls-n2">999</span>
+                        <!--end::Amount-->
+                    </div>
+                    <!--end::Info-->
+                    <!--begin::Subtitle-->
+                    <span class="text-gray-500 pt-1 fw-semibold fs-6">Employment Status</span>
+                    <!--end::Subtitle-->
+                </div>
+                <!--end::Title-->
+            </div>
+            <!--end::Header-->
+            <!--begin::Card body-->
+            <div class="card-body pt-2 pb-4 d-flex flex-wrap align-items-center">
+                <!--begin::Chart-->
+                <div class="d-flex flex-center me-5 pt-2">
+                    <div id="kt_card_widget_17_chart" style="min-width: 70px; min-height: 70px" data-kt-size="70"
+                        data-kt-line="11"></div>
+                </div>
+                <!--end::Chart-->
+                <!--begin::Labels-->
+                <div class="d-flex flex-column content-justify-center flex-row-fluid">
+                    <!--begin::Label-->
+                    <div class="d-flex fw-semibold align-items-center">
+                        <!--begin::Bullet-->
+                        <div class="bullet w-8px h-3px rounded-2 bg-success me-3"></div>
+                        <!--end::Bullet-->
+                        <!--begin::Label-->
+                        <div class="text-gray-500 flex-grow-1 me-4">Department A</div>
+                        <!--end::Label-->
+                        <!--begin::Stats-->
+                        <div class="fw-bolder text-gray-700 text-xxl-end">1</div>
+                        <!--end::Stats-->
+                    </div>
+                    <!--end::Label-->
+                    <!--begin::Label-->
+                    <div class="d-flex fw-semibold align-items-center my-3">
+                        <!--begin::Bullet-->
+                        <div class="bullet w-8px h-3px rounded-2 bg-primary me-3"></div>
+                        <!--end::Bullet-->
+                        <!--begin::Label-->
+                        <div class="text-gray-500 flex-grow-1 me-4">Department B</div>
+                        <!--end::Label-->
+                        <!--begin::Stats-->
+                        <div class="fw-bolder text-gray-700 text-xxl-end">2</div>
+                        <!--end::Stats-->
+                    </div>
+                    <!--end::Label-->
+                    <!--begin::Label-->
+                    <div class="d-flex fw-semibold align-items-center">
+                        <!--begin::Bullet-->
+                        <div class="bullet w-8px h-3px rounded-2 me-3" style="background-color: #E4E6EF"></div>
+                        <!--end::Bullet-->
+                        <!--begin::Label-->
+                        <div class="text-gray-500 flex-grow-1 me-4">Others</div>
+                        <!--end::Label-->
+                        <!--begin::Stats-->
+                        <div class="fw-bolder text-gray-700 text-xxl-end">3</div>
+                        <!--end::Stats-->
+                    </div>
+                    <!--end::Label-->
+                </div>
+                <!--end::Labels-->
+            </div>
+            <!--end::Card body-->
+        </div>
+        <!--end::Employment Status-->
+    </div>
+    <!--end::Col-->
+</div>
+<!--end::Row-->
+
+<!-- <script>
+    var ctx = document.getElementById('kt_chartjs_4');
+
+    // Define colors
+    var primaryColor = KTUtil.getCssVariableValue('--kt-primary');
+    var dangerColor = KTUtil.getCssVariableValue('--kt-danger');
+    var dangerLightColor = KTUtil.getCssVariableValue('--kt-danger-light');
+
+    // Define fonts
+    var fontFamily = KTUtil.getCssVariableValue('--bs-font-sans-serif');
+
+    // Chart labels
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    // Chart data
+    const data = {
+        labels: labels,
+        datasets: [
+            ...
+    ]
+    };
+
+    // Chart config
+    const config = {
+        type: 'line',
+        data: data,
+        options: {
+            plugins: {
+                title: {
+                    display: false,
+                },
+                legend: {
+                    labels: {
+                        // This more specific font property overrides the global property
+                        font: {
+                            size: 15,
+                            family: fontFamily
+                        }
+                    }
+                }
+            },
+            responsive: true,
+            interaction: {
+                intersect: false,
+            },
+            scales: {
+                y: {
+                    stacked: true
+                }
+            }
+        },
+        defaults: {
+            global: {
+                defaultFont: fontFamily
+            }
+        }
+    };
+
+    // Init ChartJS -- for more info, please visit: https://www.chartjs.org/docs/latest/
+    var myChart = new Chart(ctx, config);
+</script> -->
